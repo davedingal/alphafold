c86a7df42155d5e2cf5007b25d832233ff4b50e8
Polimyr Dingal <davedingal@Polimyrs-MacBook-Pro.local>
2021-08-28 11:41:41 -1000

    Adding fasta paths

--- run.sh
+++ run.sh
@@ -7,6 +7,11 @@ if ! [ -d "${DATA_DIR}" ]; then
 	exit 1
 fi
 
+if [ -z "$2" ]; then
+	echo "You must specify the fasta paths as the second argument."
+	exit 1
+fi
+
 mkdir -p output/
 ldconfig
-NVIDIA_VISIBLE_DEVICES=all TF_FORCE_UNIFIED_MEMORY=1 XLA_PYTHON_CLIENT_MEM_FRACTION=4.0 python ./run_alphafold.py --data_dir=${DATA_DIR} --uniref90_database_path=${DATA_DIR}/uniref90/uniref90.fasta --mgnify_database_path=${DATA_DIR}/mgnify/mgy_clusters_2018_12.fa --pdb70_database_path=${DATA_DIR}/pdb70/pdb70 --template_mmcif_dir=${DATA_DIR}/pdb_mmcif/mmcif_files --obsolete_pdbs_path=${DATA_DIR}/pdb_mmcif/obsolete.dat --bfd_database_path=${DATA_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt --uniclust30_database_path=${DATA_DIR}/uniclust30/uniclust30_2018_08/uniclust30_2018_08 --output_dir=./output/ --max_template_date=2020-05-14 --model_names=model_1,model_2,model_3,model_4,model_5
+NVIDIA_VISIBLE_DEVICES=all TF_FORCE_UNIFIED_MEMORY=1 XLA_PYTHON_CLIENT_MEM_FRACTION=4.0 python ./run_alphafold.py --data_dir=${DATA_DIR} --uniref90_database_path=${DATA_DIR}/uniref90/uniref90.fasta --mgnify_database_path=${DATA_DIR}/mgnify/mgy_clusters_2018_12.fa --pdb70_database_path=${DATA_DIR}/pdb70/pdb70 --template_mmcif_dir=${DATA_DIR}/pdb_mmcif/mmcif_files --obsolete_pdbs_path=${DATA_DIR}/pdb_mmcif/obsolete.dat --bfd_database_path=${DATA_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt --uniclust30_database_path=${DATA_DIR}/uniclust30/uniclust30_2018_08/uniclust30_2018_08 --output_dir=./output/ --max_template_date=2020-05-14 --model_names=model_1,model_2,model_3,model_4,model_5 --fasta_paths=$2
