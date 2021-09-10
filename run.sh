803e0c8430ccceba5d8da40495e8379fa4ac36bf
Dave Dingal <davedingal@gmail.com>
2021-09-10 00:33:38 -0400

    Adding some more log messages and initializing conda?

--- run.sh
+++ run.sh
@@ -7,10 +7,12 @@
 #SBATCH -o alphafold_%j.out # Standard out goes to this file
 #SBATCH -e alphafold_%j.err # Standard err goes to this filehostname
 
+echo "Running on host $(hostname)"
 echo "Loading anaconda"
-#module load Anaconda3/2020.11
+module load Anaconda3/2020.11
 echo "Initializing anaconda environment"
-#conda activate alphafold_env
+conda init bash
+conda activate alphafold_env
 
 DATA_DIR=$1
 
@@ -31,3 +33,5 @@ BASE_BIN_DIR=/n/home10/davedingal/.local/bin/
 # ldconfig
 echo "Running alphafold..."
 NVIDIA_VISIBLE_DEVICES=all TF_FORCE_UNIFIED_MEMORY=1 XLA_PYTHON_CLIENT_MEM_FRACTION=4.0 python ./run_alphafold.py --data_dir=${DATA_DIR} --uniref90_database_path=${DATA_DIR}/uniref90/uniref90.fasta --mgnify_database_path=${DATA_DIR}/mgnify/mgy_clusters_2018_12.fa --pdb70_database_path=${DATA_DIR}/pdb70/pdb70 --template_mmcif_dir=${DATA_DIR}/pdb_mmcif/mmcif_files --obsolete_pdbs_path=${DATA_DIR}/pdb_mmcif/obsolete.dat --bfd_database_path=${DATA_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt --uniclust30_database_path=${DATA_DIR}/uniclust30/uniclust30_2018_08/uniclust30_2018_08 --output_dir=./output/ --max_template_date=2020-05-14 --jackhmmer_binary_path=${BASE_BIN_DIR}jackhmmer --hhblits_binary_path=${BASE_BIN_DIR}hhblits --hhsearch_binary_path=${BASE_BIN_DIR}hhsearch --kalign_binary_path=${BASE_BIN_DIR}kalign --model_names=model_1,model_2,model_3,model_4,model_5 --fasta_paths=$2
+
+echo "Alphafold finished with exit status $?"
