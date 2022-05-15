070d3f81fe3885efe49795509f86b679e19e0d8f
Polimyr Dingal <ddingal@c302-002.ls6.tacc.utexas.edu>
2022-05-14 19:54:57 -0500

    getopt requires an opstring

--- run.sh
+++ run.sh
@@ -25,7 +25,8 @@ usage() {
 	exit 1
 }
 
-OPTIONS=$(getopt --long data_dir:,fasta_file:,output_dir: -- $@)
+echo $@
+OPTIONS=$(getopt --long data_dir:,fasta_file:,output_dir: -- - $@)
 eval set -- "${OPTIONS}"
 
 DATA_DIR=''
@@ -46,11 +47,17 @@ while true ; do
   esac
 done
 
+if [ -z "${FASTA_FILE}" -o -z "${DATA_DIR}" -o -z "${OUTPUT_DIR}" ]; then
+  usage
+  exit 1
+fi
+
 mkdir -p "${OUTPUT_DIR}"
 
 BASE_BIN_DIR=${HOME}/.local/bin/
 
 # ldconfig
+echo "Fasta file: ${FASTA_FILE}"
 echo "Running alphafold..."
 NVIDIA_VISIBLE_DEVICES=all TF_FORCE_UNIFIED_MEMORY=1 XLA_PYTHON_CLIENT_MEM_FRACTION=4.0 python ./run_alphafold.py --data_dir=${DATA_DIR} --uniref90_database_path=${DATA_DIR}/uniref90/uniref90.fasta --mgnify_database_path=${DATA_DIR}/mgnify/mgy_clusters_2018_12.fa --pdb70_database_path=${DATA_DIR}/pdb70/pdb70 --template_mmcif_dir=${DATA_DIR}/pdb_mmcif/mmcif_files --obsolete_pdbs_path=${DATA_DIR}/pdb_mmcif/obsolete.dat --bfd_database_path=${DATA_DIR}/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt --uniclust30_database_path=${DATA_DIR}/uniclust30/uniclust30_2018_08/uniclust30_2018_08 --pdb_seqres_database_path=${DATA_DIR}/pdb_seqres/pdb_seqres.txt --uniprot_database_path=${DATA_DIR}/uniprot/uniprot.fasta --small_bfd_database_path=${DATA_DIR}/small_bfd/bfd-first_non_consensus_sequences.fasta --output_dir=${OUTPUT_DIR} --max_template_date=2020-05-14 --jackhmmer_binary_path=${BASE_BIN_DIR}jackhmmer --hhblits_binary_path=${BASE_BIN_DIR}hhblits --hhsearch_binary_path=${BASE_BIN_DIR}hhsearch --kalign_binary_path=${BASE_BIN_DIR}kalign --fasta_paths=${FASTA_FILE} --logtostderr --use_precomputed_msas=true --use_gpu_relax=true $@
 
